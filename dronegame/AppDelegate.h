1e358a7ec8bc60873c688a4f4d93ccd1755ebd69
Steven Blessing <sbless@sbless-macbookpro.fios-router.home>
2017-07-15 19:38:41 -0400

    Initial Commit

---
+++ dronegame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dronegame
+//
+//  Created by Steven Blessing on 7/15/17.
+//  Copyright © 2017 ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
